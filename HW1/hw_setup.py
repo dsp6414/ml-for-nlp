c76defdf4de7ca847ea65f1196e08ab7e50c5688
xususan <susan.xu97@gmail.com>
2018-01-28 20:25:49 -0500

    added more stuff to hw_setup

--- HW1/hw_setup.py
+++ HW1/hw_setup.py
@@ -1,4 +1,3 @@
-
 # Text text processing library and methods for pretrained word embeddings
 import torchtext
 from torchtext.vocab import Vectors, GloVe
@@ -13,5 +12,14 @@ train, val, test = torchtext.datasets.SST.splits(
     TEXT, LABEL,
     filter_pred=lambda ex: ex.label != 'neutral')
 
-print(val[:5])
-print(train:size())
+TEXT.build_vocab(train)
+LABEL.build_vocab(train)
+
+
+train_iter, val_iter, test_iter = torchtext.data.BucketIterator.splits(
+    (train, val, test), batch_size=10, device=-1)
+
+batch = next(iter(train_iter))
+print("Size of text batch [max sent length, batch size]", batch.text.size())
+print("Second in batch", batch.text[:, 0])
+print("Converted back to string: ", " ".join([TEXT.vocab.itos[i] for i in batch.text[:, 0].data]))
