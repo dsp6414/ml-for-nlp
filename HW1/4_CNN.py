798d1837d9d7462d69556b8c613855b7c19c6ddf
Anna Liu <yuhuiliu1@gmail.com>
2018-01-31 16:48:29 -0500

    Updating 4

--- HW1/4_CNN.py
+++ HW1/4_CNN.py
@@ -42,12 +42,9 @@ class CNN(nn.Module):
         self.model = model
         self.embedding = nn.Embedding(vocab_size+2, embedding_dim, padding_idx=vocab_size+1)
 
-        # Go and understand this
-        self.conv = nn.ModuleList([nn.Conv2d(self.in_channel, self.out_channel, (F, embedding_dim), stride=embedding_dim) for F in filter_windows])
+        self.conv = nn.ModuleList([nn.Conv2d(self.in_channel, self.out_channel, (F, embedding_dim), padding=2) for F in filter_windows])
         # self.conv = nn.ModuleList([nn.Conv1d(self.in_channel, self.out_channel, embedding_dim * F, stride=embedding_dim) for F in filter_windows])
         self.dropout = nn.Dropout(dropout)
-
-        # Go and understand this
         self.fc = nn.Linear(len(filter_windows) * self.out_channel, class_number) # Fully connected layer
 
         if model == "static":
@@ -58,57 +55,53 @@ class CNN(nn.Module):
             in_channel = 2
 
     def convolution_max_pool(self, inputs, convolution, i, max_sent_len):
+        ############ OLD CODE
         # result_convolution = F.relu(convolution(inputs))
-        # result = F.max_pool1d(result_convolution, max_sent_len - self.filter_windows[i] + 1).view(-1, self.out_channel)
-        
-        # OLD CODE
-        # pdb.set_trace()
+        # result = F.max_pool1d(result_convolution, result_convolution.size(2)).squeeze(2)
+        # return result
+        ############
 
+        # CODE THAT WORKS
+        ########
+        pdb.set_trace()
         result_convolution = F.relu(convolution(inputs)).squeeze(3) # (batch_size, out_channel, max_seq_len)
         
-        # pdb.set_trace()
+        pdb.set_trace()
         result = F.max_pool1d(result_convolution, result_convolution.size(2)).squeeze(2) # (batch_size, out_channel)
         return result
+        ##########
 
     def forward(self, inputs):
-        max_sent_len = inputs.size(1)
 
+        # CODE THAT WORKS
+        ########
+        max_sent_len = inputs.size(1)
         embedding = self.embedding(inputs) # (batch_size, max_seq_len, embedding_size)
         if self.model == "multichannel":
             embedding2 = self.embedding2(inputs)
             embedding = torch.cat((embedding, embedding2), 1)
 
-        embedding = embedding.unsqueeze(1) # () might be a problem in multichannel
+        embedding = embedding.unsqueeze(1) #
 
-        # embedding = self.embedding(inputs).view(-1, 1, self.embedding_dim * inputs.size(1))
 
-        # if self.model == "multichannel":
-            # embedding2 = self.embedding2(inputs).view(-1, 1, self.embedding_dim * inputs.size(1))
-            # embedding = torch.cat((embedding, embedding2), 1)
 
-        # pdb.set_trace()
         result = [self.convolution_max_pool(embedding, k, i, max_sent_len) for i, k in enumerate(self.conv)]
-        
-        # pdb.set_trace()
         result = self.fc(self.dropout(torch.cat(result, 1)))
-
         return result
+        ##########
 
         # OLD CODE
+        ###################
+        # max_sent_len = inputs.size(1)
+        # embedding = self.embedding(inputs).view(-1, 1, self.embedding_dim * max_sent_len)
+        # if self.model == "multichannel":
+        #     embedding2 = self.embedding2(inputs).view(-1, 1, self.embedding_dim * inputs.size(1))
+        #     embedding = torch.cat((embedding, embedding2), 1)
 
-        # x = [F.relu(conv(embedding)).squeeze(3) for conv in self.conv]
-        # x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x] # Passing in max_sentence_length - filter_window + 1
-        # result = self.fc(self.dropout(torch.cat(x, 1)))
-        # return result 
-
-parser = argparse.ArgumentParser(description='Text Classification through CNN')
-parser.add_argument('-lr', type=float, default=1e-1, help='setting learning rate')
-parser.add_argument('-embedding-dim', type=int, default=128, help='number of embedding dimension [default: 128]')
-parser.add_argument('-feature-maps', type=int, default=100, help='number of feature maps')
-parser.add_argument('-filter-windows', type=str, default='3,4,5', help='comma-separated filter windows to use for convolution')
-parser.add_argument('-dropout', type=float, default=0.5, help='the probability for dropout [default: 0.5]')
-parser.add_argument('-max-norm', type=float, default=3.0, help='l2 constraint of parameters [default: 3.0]')
-args = parser.parse_args()
+        # result = [self.convolution_max_pool(embedding, k, i, max_sent_len) for i, k in enumerate(self.conv)]
+        # result = self.fc(self.dropout(torch.cat(result, 1)))
+        # return result
+        ###################
 
 if __name__ == '__main__':
     EMBEDDING_SIZE = 128
@@ -138,21 +131,26 @@ if __name__ == '__main__':
     optimizer = optim.SGD(net.parameters(), lr=0.1)
 
     for epoch in range(1):
+        counter = 0
         total_loss = 0
         for batch in train_iter:
+            counter += 1
             text, label = batch.text.t_(), batch.label
             # if len(text) < max(self.filter_windows):
             #     text = F.pad(text, (1, ceiling((max(self.filter_windows)-len(text))/2)), value=) # FINISH THIS PADDING
             label = label - 1
             net.zero_grad()
 
-            # pdb.set_trace()
-            # Figure out if this is the right call
             logit = net(text)
+
+            # pdb.set_trace()
             loss = criterion(logit, label)
             loss.backward()
             optimizer.step()
 
+
+            if counter > 40:
+                break
             total_loss += loss.data
         print("loss =", total_loss)
 
