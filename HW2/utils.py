1cb53f8fb8feaccfe67610b908a25cd92f136103
xususan <susan.xu97@gmail.com>
2018-02-08 22:03:50 -0500

    before fixing validate

--- HW2/utils.py
+++ HW2/utils.py
@@ -9,23 +9,15 @@ def validate(model, val_iter):
 	total  = 0.0
 	last_pred = None
 	for batch in val_iter:
-		x = batch.text
+		# Take every row except bottom row = last word in each obs.
+		x = batch.text[:-1,:]
+		y = batch.text[-1, :].squeeze()
 		probs = model(x)
 		_, preds = torch.max(probs, 1)
 
-		# cut off the first target unless there is a previous prediction
-		targets = x[0, :] if last_pred is not None else x[0, 1:]
-
-		# cut off the last prediction for now, use it later
-		current_preds = preds[:-1]
-		current_preds = torch.cat([last_pred, current_preds]) if last_pred is not None else current_preds
-
-		# Use the last prediction in the next row
-		last_pred = preds[-1:]
-
-		correct += sum(current_preds == targets.data)
+		correct += sum(preds == y.data)
 		total += batch.text.size()[1] - 1
-		print(current_preds, targets)
+		print(preds, y)
 	print(correct,total)
 	return correct / total
 
