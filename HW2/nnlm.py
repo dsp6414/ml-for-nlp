79fcc290ab778ab8e72e948a58633ca58ea7724d
xususan <susan.xu97@gmail.com>
2018-02-11 16:26:01 -0500

    wtf why

--- HW2/nnlm.py
+++ HW2/nnlm.py
@@ -32,7 +32,7 @@ class LSTMLM(nn.Module):
 		print("pre emedding")
 		word_vectors = self.embedding(batch) # [n x embedding_dim]
 		print('hello')
-		print("w,h", word_vectors.size(), h.size())
+		print("w,h", word_vectors.size(), h[0].size(), h[1].size())
 		
 		# Get predictions and hidden state from LSTM  
 		# print(self.lstm(word_vectors, h)
