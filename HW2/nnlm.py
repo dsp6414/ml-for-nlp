cfbaa2c80e8de55ff4d9621a4b42976f70276459
xususan <susan.xu97@gmail.com>
2018-02-11 16:24:41 -0500

    wtf is wrong with this batch size

--- HW2/nnlm.py
+++ HW2/nnlm.py
@@ -32,10 +32,10 @@ class LSTMLM(nn.Module):
 		print("pre emedding")
 		word_vectors = self.embedding(batch) # [n x embedding_dim]
 		print('hello')
-		print(word_vectors, h)
+		print("w,h", word_vectors.size(), h.size())
 		
 		# Get predictions and hidden state from LSTM  
-		# print(self.lstm(word_vectors, h))
+		# print(self.lstm(word_vectors, h)
 		out, h = self.lstm(word_vectors, h) # out is [n, 1, hidden_size]
 		# print("result of lstm", out.size())
 		
