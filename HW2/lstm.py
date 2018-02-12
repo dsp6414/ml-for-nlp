d5c2cb93ddd4ca42d4eb92537cd1ad0de90489a6
Anna Liu <yuhuiliu1@gmail.com>
2018-02-12 03:46:09 -0500

    extension

--- HW2/lstm.py
+++ HW2/lstm.py
@@ -9,6 +9,7 @@ import torchtext
 from torchtext.vocab import Vectors, GloVe
 import pickle
 import utils
+import math
 
 import pdb
 
@@ -40,23 +41,6 @@ INIT_PARAM = 0.05
 # INIT_PARAM = 0.04
 
 # PARSE ARGS
-
-# TEXT = torchtext.data.Field()
-# # Data distributed with the assignment
-# train, val, test = torchtext.datasets.LanguageModelingDataset.splits(
-#     path=".", 
-#     train="train.5k.txt", validation="valid.txt", test="valid.txt", text_field=TEXT)
-# TEXT.build_vocab(train)
-# if args.mini:
-#     TEXT.build_vocab(train, max_size=1000)
-# train_iter, val_iter, test_iter = torchtext.data.BPTTIterator.splits(
-#     (train, val, test), batch_size=BATCH_SIZE, device=-1, bptt_len=UNROLL, repeat=False)
-# url = 'https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.simple.vec'
-# TEXT.vocab.load_vectors(vectors=Vectors('wiki.simple.vec', url=url))
-
-# VOCAB_SIZE = len(TEXT.vocab)
-
-# print("vocab size: " + str(VOCAB_SIZE))
 
 class LSTM(nn.Module):
     def __init__(self, embedding_size, vocab_size, num_layers=2, lstm_type='medium'):
@@ -106,100 +90,89 @@ class LSTM(nn.Module):
         # output.view(batch_size, -1)
         return output, hidden
 
-# rnn = LSTM(embedding_size=EMBEDDING_SIZE , vocab_size=VOCAB_SIZE, hidden_size=self.hidden_size, num_layers=NUM_LAYERS, dropout=DROPOUT)
-# criterion = nn.CrossEntropyLoss()
+# def train_batch(model, criterion, optim, text, target, epoch):
+#     # initialize hidden vectors
+#     hidden = model.init_hidden() # This includes (hidden, cell)
 
-# optimizer = optim.SGD(rnn.parameters(), lr=LR/DECAY)
-# optimizer = optim.Adadelta(rnn.parameters(), lr=LR/DECAY)
-# scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[TEMP_EPOCH], gamma=1/DECAY)
+#     # clear gradients
+#     model.zero_grad()
+#     # calculate forward pass
+#     output, hidden = model(text, hidden)
+#     # calculate loss
+#     output_flat = output.view(-1, VOCAB_SIZE)
+#     loss = criterion(output_flat, target) # output: [bptt_len-1 x batch x vocab_size]
+#     # target: [bptt_len-1 x batch]
+#     # backpropagate and step
+#     loss.backward()
+#     nn.utils.clip_grad_norm(model.parameters(), max_norm=GRAD_NORM)
+#     optimizer.step()
+#     return loss.data[0]
 
-def train_batch(model, criterion, optim, text, target, epoch):
-    # initialize hidden vectors
-    hidden = model.init_hidden() # This includes (hidden, cell)
+# def train(model, criterion, optim):
+#     model.train()
+#     for epoch in range(EPOCHS):
+#     # for epoch in range(12):
+#         total_loss = 0
+#         counter = 0
+#         print(sum(1 for _ in train_iter))
+#         for batch in train_iter:
+#             text, target = utils.get_batch(batch, )
 
-    # clear gradients
-    model.zero_grad()
-    # calculate forward pass
-    output, hidden = model(text, hidden)
-    # calculate loss
-    output_flat = output.view(-1, VOCAB_SIZE)
-    loss = criterion(output_flat, target) # output: [bptt_len-1 x batch x vocab_size]
-    # target: [bptt_len-1 x batch]
-    # backpropagate and step
-    loss.backward()
-    nn.utils.clip_grad_norm(model.parameters(), max_norm=GRAD_NORM)
-    optimizer.step()
-    return loss.data[0]
+#             batch_loss = train_batch(model, criterion, optim, text, target, epoch)
+#             total_loss += batch_loss
+#             # print(str(counter) + "   " + str(total_loss))
+#             counter += 1
+#         scheduler.step()
+#         print("learning rate: " + str(scheduler.get_lr()))
+#         print("Epoch " + str(epoch) + " Loss: " + str(total_loss))
+#         print(rnn)
 
-def train(model, criterion, optim):
-    model.train()
-    for epoch in range(EPOCHS):
-    # for epoch in range(12):
-        total_loss = 0
-        counter = 0
-        print(sum(1 for _ in train_iter))
-        for batch in train_iter:
-            text, target = utils.get_batch(batch, )
+class LSTMExtension(nn.Module):
+    def __init__(self, embedding_size, vocab_size, num_layers=2, lstm_type='medium'):
+        super(LSTMExtension, self).__init__()
+        self.lstm_type = lstm_type
+        self.embedding_size = 400
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
 
-            batch_loss = train_batch(model, criterion, optim, text, target, epoch)
-            total_loss += batch_loss
-            # print(str(counter) + "   " + str(total_loss))
-            counter += 1
-        scheduler.step()
-        print("learning rate: " + str(scheduler.get_lr()))
-        print("Epoch " + str(epoch) + " Loss: " + str(total_loss))
-        print(rnn)
+        self.hidden_size = 1150
+        self.init_param = 1.0 / math.sqrt(self.hidden_size)
+        self.init_param_embedding = 0.01
+        self.dropout = 0.3
 
-def evaluate(model, val_iter, hidden=False):
-    # correct = 0.0
-    # total  = 0.0
-    # num_zeros = 0.0
-    total_loss = 0.0
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, self.hidden_size // 2, num_layers, dropout=self.dropout, bidirectional=True)
+        self.linear = nn.Linear(self.hidden_size, vocab_size)
+        self.dropout_embedding = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(self.dropout)
+        self.init_weights()
 
-    model.eval()
+    def init_weights(self):
+        self.embedding.weight.data.uniform_(-self.init_param_embedding, self.init_param_embedding)
+        self.linear.weight.data.uniform_(-self.init_param, self.init_param)
 
-    # if hidden
-    h = model.init_hidden()
-
-    for batch in val_iter:
-        text = batch.text[:-1,:]
-        target = batch.text[1:,:].view(-1)
-        
+    def init_hidden(self, num_directions=2, batch_size=40):
         if torch.cuda.is_available():
-            text = text.cuda()
-            target = target.cuda()
+            return (Variable(torch.zeros(self.num_layers*num_directions, batch_size, self.hidden_size // num_directions)).cuda(),
+            Variable(torch.zeros(self.num_layers*num_directions, batch_size, self.hidden_size // num_directions)).cuda())
 
-        # if hidden:
-        probs, h = model(text, h)
-        probs_flat = probs.view(-1, VOCAB_SIZE)
+        return (Variable(torch.zeros(self.num_layers*num_directions, batch_size, self.hidden_size // num_directions)),
+            Variable(torch.zeros(self.num_layers*num_directions, batch_size, self.hidden_size // num_directions)))
 
-        total_loss += criterion(probs_flat, target).data
-        # else:
-        #     probs = model(text)
+    def set_weights(self, weights='weight_hh_l0', dropout=0.25):
+        w = F.dropout(self.rnn.weight_hh_l0, p=dropout, training=True)
 
-        # _, preds = torch.max(probs, 1)
-        # print(probs, target)
-        # correct += sum(preds.view(-1, len(TEXT.vocab)) == target.data)
-        # total += 1
-        # num_zeros += sum(torch.zeros_like(target.data) == target.data)
+    def forward(self, inputs, hidden):
+        self.set_weights()
+        embedding = self.dropout_embedding(self.embedding(inputs)) # [bptt_len - 1 x batch x embedding_size]
+        # embedding = embedding.transpose(0, 1)
+        # hidden size: [layers x batch x units]
+        output, hidden = self.rnn(embedding, hidden) # [bptt_len - 1 x batch x units]
+        # embedding..view(1, batch_size, -1) without transpose
+        output = self.dropout(output)
+        output = self.linear(output.view(-1, self.hidden_size)) # [bptt_len - 1 x batch x vocab_size]
+        # output.view(batch_size, -1)
+        return output, hidden
 
-    print(total_loss[0])
-    return total_loss
 
-####### CHECK FOR CUDA
-# if torch.cuda.is_available():
-#     print("USING CUDA")
-#     rnn = rnn.cuda()
-#######
-
-# train(rnn, criterion, optimizer)
-
-# filename = 'lstm_model.sav'
-# pickle.dump(rnn, open(filename, 'wb'))
-
-# loaded_model = pickle.load(open(filename, 'rb'))
-# print("Validation Set")
-# evaluate(loaded_model, val_iter, hidden=True)
-# print("Test Set")
-# evaluate(loaded_model, test_iter, hidden=True)
 
