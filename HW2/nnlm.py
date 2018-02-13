12a1198c4335bdca755d4d3f45814aeb97dba5a9
xususan <susan.xu97@gmail.com>
2018-02-13 10:22:14 -0500

    two layers

--- HW2/nnlm.py
+++ HW2/nnlm.py
@@ -18,7 +18,7 @@ class LSTMLM(nn.Module):
 		self.dropout = nn.Dropout(DROPOUT)
 		self.linear = nn.Linear(hidden_size * n, vocab_size)
 		self.linear_two = nn.Linear(self.embedding_dim * n, vocab_size)
-		self.linear_three = nn.Linear(self.embedding_dim * n, vocab_size)
+		self.inner_lin = nn.Linear(self.embedding_dim * n, self.embedding_dim * n)
 		self.init_weights()
 		self.initial_hidden = None #autograd.Variable(torch.zeros(1, hidden_size))
 		self.is_eval = False
@@ -53,12 +53,14 @@ class LSTMLM(nn.Module):
 		out = word_vectors.t().contiguous().view(1, -1, self.n * self.embedding_dim)
 		# print(reshaped.size())
 
+
+		out = self.tanh(out)
+		out = self.inner_lin(out)
 		out = self.tanh(out)
 		out = self.linear_two(out)
 
-		out = self.tanh(out)
+		
 
-		out = self.linear_three(out)
 
 
 		## out, h = self.lstm(reshaped, h) # out is [n, 1, hidden_size]
