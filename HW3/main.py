98c1ae4f3b67808643e94f4b9041571486341f5a
xususan <susan.xu97@gmail.com>
2018-03-02 12:03:28 -0500

    trace

--- HW3/main.py
+++ HW3/main.py
@@ -128,5 +128,5 @@ else:
 
 print("KAGGLE")
 # can use beam here
-output_name = filename[:-4] + '_preds.txt'
+output_name = filename[:-4] + '_preds.torchtext'
 utils.kaggle(model, SRC_LANG=DE, TRG_LANG=EN, output_file="seq2seq_preds.txt", input_file="source_test.txt")
