811ff3403d3f75ceb82a2965e50bf7440e86a2b5
xususan <susan.xu97@gmail.com>
2018-03-02 04:25:12 -0500

    hope

--- HW3/utils.py
+++ HW3/utils.py
@@ -193,7 +193,7 @@ def kaggle(model, SRC_LANG, TRG_LANG, output_file, input_file='source_test.txt')
             if USE_CUDA:
                 text = text.cuda()
             sequences = model(text, None, k=5, use_target=False) # THE ONLY TIME USE_TARGET = FALSE
-            
+            print(sequences)
             # convert each seq to sentence
             print("%d,", i, end='', file=out)
             for sequence in sequences:
