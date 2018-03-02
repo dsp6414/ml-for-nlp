5cf1df47633aa7861a3b9c8a6bf2258221255959
xususan <susan.xu97@gmail.com>
2018-03-02 12:10:03 -0500

    removed sort

--- HW3/model.py
+++ HW3/model.py
@@ -585,7 +585,7 @@ class Seq2Seq(nn.Module):
                         new_probs = F.log_softmax(n_probs, dim=2) + log_prob# this should be tensor of size k 
                         new_probs = new_probs.squeeze().data
                         pdb.set_trace()
-                        new_sequences = [torch.cat([last_sequence_guess, n_index.view(1, 1)],dim=0) for n_index in n_indices.squeeze()] # check this
+                        new_sequences = [torch.cat([last_sequence_guess, n_index.view(1, 1)],dim=0) for n_index in n_indices.transpose(0, 2)] # check this
                         new_hidden = [decoder_hidden] * k
                         # decoder_hidden: # tuple, each of which is [num_layers x batch x hidden]
                         assert(len(new_sequences) == k)
