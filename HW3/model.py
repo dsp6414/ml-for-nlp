8c200d102830e882b92bb6e8344ae879243bd5ab
xususan <susan.xu97@gmail.com>
2018-03-03 22:42:31 -0500

    more clean

--- HW3/model.py
+++ HW3/model.py
@@ -17,6 +17,8 @@ USE_CUDA = True if torch.cuda.is_available() else False
 MAX_LEN = 20
 BOS_EMBED = 2
 EOS_EMBED = 3
+DE_VOCAB_SIZE = 13353
+EN_VOCAB_SIZE = 11560
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, embedding_size, hidden_size, n_layers=1, dropout_p=0.5):
         super(EncoderRNN, self).__init__()
@@ -90,11 +92,8 @@ class AttnDecoderRNN(nn.Module):
         self.out = nn.Linear(hidden_size * 2, output_size)
         self.attn = True
 
-        # inputs is the true values for the target sentence from previous time step
-        # last_hidden is the bottleneck hidden from processing all of encoder
-        # encoder_outputs is
+
     def forward(self, target, last_hidden, encoder_outputs):
-        # check: target is (seq_len, batch, input_size)
         if len(target.size()) == 1:
             target = target.unsqueeze(0)
         word_embeddings = self.embedding(target) # [seq_len x B x E]
@@ -103,7 +102,6 @@ class AttnDecoderRNN(nn.Module):
         attn_weights = F.softmax(scores, dim=1) # [B x source_len x target_len]
         context = torch.bmm(attn_weights.transpose(1, 2), encoder_outputs.transpose(0, 1))
         output = self.out(torch.cat((decoder_outputs.transpose(0, 1), context), 2))
-
         output = output.transpose(0, 1).contiguous() # [Seq_len x B x en_vocab]
 
         return output, hidden, attn_weights
@@ -140,10 +138,10 @@ class Seq2Seq(nn.Module):
 
         decoder_hidden = tuple([e.view(self.n_layers, e.size(1), -1) for e in encoder_hidden]) # [num_layers x batch x hidden]. 
 
-        # THE REAL KAGGLE THING
+        # For Kaggle predictions.
         if self.valid:
             decoder_hidden = encoder_hidden 
-            initial_guess = Variable(torch.LongTensor([2]).view(1, 1))
+            initial_guess = Variable(torch.LongTensor([BOS_EMBED]).view(1, 1))
             if USE_CUDA:
                 initial_guess = initial_guess.cuda()
             current_hypotheses = [(0, initial_guess, decoder_hidden)]
@@ -170,19 +168,17 @@ class Seq2Seq(nn.Module):
                             decoder_outputs, decoder_hidden = self.decoder(last_word, decoder_hidden, encoder_outputs)
                         # Get k hypotheses for each 
                         # decoder outputs is [target_len x batch x en_vocab_sz] -> [1 x 1 x vocab]
-                        vocab_size = len(decoder_outputs[0][0])
+                        vocab_size = EN_VOCAB_SIZE
                         n_probs, n_indices = torch.topk(decoder_outputs, k, dim=2)
                         new_probs = F.log_softmax(n_probs, dim=2) + log_prob# this should be tensor of size k 
                         new_probs = new_probs.squeeze().data
                         new_sequences = [torch.cat([last_sequence_guess, n_index.view(1, 1)],dim=0) for n_index in n_indices.squeeze()] # check this
                         new_hidden = [decoder_hidden] * k
-                        # decoder_hidden: # tuple, each of which is [num_layers x batch x hidden]
                         seq_w_probs = list(zip(new_probs, new_sequences, new_hidden))
                         guesses_for_this_length = guesses_for_this_length + seq_w_probs
 
                 # Top k current hypotheses after this time step:
                 guesses_for_this_length = sorted(guesses_for_this_length, key= lambda tup: -1*tup[0])[:k]
-
                 current_hypotheses = current_hypotheses + guesses_for_this_length
 
             # Return top result
