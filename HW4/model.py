143e98d487e9c3fe54fce373d224b2c100083206
xususan <susan.xu97@gmail.com>
2018-03-22 20:54:05 -0400

    forgot to divide

--- HW4/model.py
+++ HW4/model.py
@@ -49,14 +49,12 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.fc1 = nn.Linear(input_size, 256)
         self.fc2 = nn.Linear(self.fc1.out_features, 512)
-        self.fc3 = nn.Linear(self.fc2.out_features, 1024)
-        self.fc4 = nn.Linear(self.fc3.out_features, output_size)
+        self.fc4 = nn.Linear(self.fc2.out_features, output_size)
 
     # forward method
     def forward(self, input):
         x = F.leaky_relu(self.fc1(input), 0.2)
         x = F.leaky_relu(self.fc2(x), 0.2)
-        x = F.leaky_relu(self.fc3(x), 0.2)
         x = F.tanh(self.fc4(x))
         return x
 
