f8099fe065be285c4399c7fe6ea6a425423fbfe1
Anna Liu <yuhuiliu1@gmail.com>
2018-04-24 16:40:39 -0400

    working through fixing model

--- Final/main.py
+++ Final/main.py
@@ -23,22 +23,31 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
+parser.add_argumet('--hidden-sz', type=int, default=100,
+                    help='hidden size of the neural networks')
 args = parser.parse_args()
 
 torch.manual_seed(args.seed)
 
+
+WORD_EMBEDDING_SZ = 50
+PROP_EMBEDDING_SZ = 50
+
+RHO = 0.95
+EPS = 0.000001
+LR = 1
+CLIP = 10
+
 N_TEST_IMAGES = 100
 N_TEST = N_TEST_IMAGES * 10
-
 N_EXPERIMENT_PAIRS = 100
 
 train_scenes, dev_scenes, test_scenes = corpus.load_abstract()
 
-
-listener0_model = Listener0Model() # need to pass in some parameters
-speaker0_model = Speaker0Model()
-sampling_speaker1_model = SamplingSpeaker1Model()
-# compiled_speaker1_model = Compiledspeaker1Model()
+listener0_model = model.Listener0Model(args.hidden_sz) # need to pass in some parameters
+speaker0_model = model.Speaker0Model(args.hidden_sz)
+sampling_speaker1_model = model.SamplingSpeaker1Model(args.hidden_sz)
+# compiled_speaker1_model = model.Compiledspeaker1Model()
 
 # Train base
 utils.train(train_scenes, dev_scenes, listener0_model)
